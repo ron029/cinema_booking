1bf11130cf01138cda1fe8561681f8e2932307c8
ron029 <bonifacioronmark@gmail.com>
2023-08-09 07:46:44 +0800

    downgrade bootstrap from 5.3.1 to 3.4.1

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ ruby '2.6.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.0.6', '>= 6.0.6.1'
-
+gem 'bootstrap-sass', '3.4.1'
 gem 'kaminari'
 
 gem 'ransack'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("bootstrap")
-import 'jquery/src/jquery'
-import './src/application.scss';
-import './custom'
+rrequire('jquery')
+import 'bootstrap'
+
